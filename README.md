bb03010fcf37f5aa50629f049187c1418be9f5df
anonymousskt <77140403+anonymousskt@users.noreply.github.com>
2023-02-22 17:30:13 -0300

    aula2

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # intro-JavaScript
  Introdução Programação JavaScript
 ## <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/Hi.gif" width="29px"> FEITO POR: RAUNYKKJ
-<p align="center">
+<p>
 <img src="https://media-giphy-com.cdn.ampproject.org/ii/w820/s/media.giphy.com/media/1g3A0gpaidxWcL9Mfo/giphy.gif" width="230" height="230"/>
 </p>
 <br>
--- exemplos/000-javascript.js
+++ exemplos/000-javascript.js
@@ -1,6 +1,12 @@
 // o comando abaixo apresenta uma mensagem na tela
 console.log('Olá Estou Programando Em JavaScript');
 
+
+
+//Comentario Que Ocupa Uma Linha
+
+
+
 /*
     comentário que ocupa mais 
     de uma linha 
@@ -8,23 +14,80 @@ console.log('Olá Estou Programando Em JavaScript');
 
 // criação de variável do tipo texto
 let nome = 'Rauny Aruam De Oliveira Reis';
+
 // criação de variável do tipo numérico (inteiro)
-let idade = '16';
+let idade = 16;
+
 // criação de variável do tipo númerico (decimal)
-let altura = '1,65';
+let altura = 1.65 ;
+
 // criação de variável do tipo verdadeiro/falso
 let trabalha = true; // true = verdadeiro | false = falso
+
 // exemplo de variável de nome composto
 let corFavorita = 'laranja';
+
 // exemplo de variável do tipo lista de valores
-let cores_favoritas = ['preto','branco','vermelho'];
+let cores_favoritas = ['preto', 'branco', 'vermelho'];
+
 // exemplo de variável que armazena um objeto
 let endereço = {
     'logradouro'        : 'Rua',
     'nome_logradouro'   : 'Brasil',
     'número'            : '951 B',
     'bairro'            : 'Parque Ipiranga',
-    'cidade'            : 'Tupã'
+    'cidade'            : 'Tupã',
 };
+
 let covid;
 let dengue = null;
+let time = 'Palmeiras Melhor Do Mundo';
+
+
+console.log('Nome: ' + nome);
+console.log('Tipo Da Variavel: ' + typeof(nome));
+
+
+
+console.log('Idade: ' + idade);
+console.log('Tipo Da Variavel: ' + typeof(idade));
+
+
+
+console.log('Altura: ' + altura);
+console.log('Tipo Da Variavel: ' + typeof(altura));
+
+
+
+console.log('Trabalha :' + trabalha);
+console.log('Tipo Da Variavel: ' + typeof(trabalha));
+
+
+
+console.log('Cor Favorita' + corFavorita);
+console.log('Tipo Da Variavel: ' + typeof(corFavorita));
+
+
+
+console.log('Cores Favoritas: ' + cores_favoritas);
+console.log('Tipo Da Variavel: ' + typeof(cores_favoritas));
+
+
+
+console.log('Endereço : ' + endereço);
+console.log('Tipo Da Variavel: ' + typeof(endereço));
+
+
+
+console.log('Covid: ' + covid);
+console.log('Tipo Da Variavel: ' + typeof(covid));
+
+
+
+console.log('Dengue: ' + dengue);
+console.log('Tipo Da Variavel: ' + typeof(dengue));
+
+
+
+console.log('Time: ' + time);
+console.log('Tipo Da Variavel: ' + typeof(time));
